2019e9150ccf243979a8acfe13e38eb3103b784e
ShaikAfridh <86220601+ShaikAfridh@users.noreply.github.com>
2022-11-11 00:06:45 -0500

    Add files via upload

--- Projects/add-local-user.sh
+++ Projects/add-local-user.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
 
+if [[ "${UID}" != '0' ]]; then
+  echo 'please run script sudo as a root'
+  exit 1
+fi
+
+read -p 'Enter the username: ' username
+read -p 'Enter your name: ' comment
+read -p 'Enter the password: ' password
+
+useradd -c ${comment} -m ${username}
+
+if [[ "${?}" -ne 0 ]]; then
+  echo 'Something went wrong account is not able to create.'
+  exit 1
+fi
+
+echo ${password} |passwd --stdin ${username}
+
+passwd -e ${username}
+
+
+echo "USERNAME : ${username}"
+echo "PASSWORD : ${password}"
+echo "HOSTNAME : ${HOSTNAME}"
